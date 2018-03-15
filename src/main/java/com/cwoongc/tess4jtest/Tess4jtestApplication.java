12e2055fabf46d02d1f7a3b59159c7f6e06082fc
cwoongc <cwoongc@gmail.com>
2018-03-15 18:16:28 +0900

    more profiles

--- src/main/java/com/cwoongc/tess4jtest/Tess4jtestApplication.java
+++ src/main/java/com/cwoongc/tess4jtest/Tess4jtestApplication.java
@@ -8,13 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import javax.imageio.stream.FileImageInputStream;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 
 @SpringBootApplication
 public class Tess4jtestApplication {
@@ -24,8 +22,9 @@ public class Tess4jtestApplication {
 	}
 
 
+	@Profile({"doOCR"})
 	@Bean
-	CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+	CommandLineRunner doOCR(ApplicationContext ctx) {
 		return args -> {
 
 			File imgFile = new File(getClass().getResource("image007.png").getFile());
@@ -37,7 +36,7 @@ public class Tess4jtestApplication {
 			ITesseract instance = new Tesseract();
 
 			try {
-				instance.setLanguage("kor");
+				instance.setLanguage("kor+eng");
 
 				String result = instance.doOCR(imgFile);
 				System.out.println(result);
@@ -45,6 +44,65 @@ public class Tess4jtestApplication {
 				System.err.println(e.getMessage());
 			}
 
+		};
+	}
+
+	@Profile({"see-unicode"})
+	@Bean
+	CommandLineRunner seeUnicode(ApplicationContext ctx) {
+		return args -> {
+			try(
+			BufferedWriter br = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("hangul.txt",false),"UTF-8"))
+			){
+				for(int c=0xAC00,i=0;c<=0xD7A3;c++,i++) {
+
+					if(i%38==0 && i!=0) br.write(System.lineSeparator());
+					else if(i!=0) br.write(' ');
+
+					br.write((char)c);
+
+					if(i%101 == 0) br.flush();
+				}
+
+				br.flush();
+			}
+		};
+	}
+
+	@Profile({"split-43"})
+	@Bean
+	CommandLineRunner split43(ApplicationContext ctx) {
+		return args -> {
+				File uniFile = new File(getClass().getResource("uni.txt").getFile());
+				try(
+				BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(uniFile),"UTF-8"));
+				){
+
+					String c = null;
+					int i = 0;
+					int docNo = 0;
+
+					BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("unichar/h"+docNo+".txt")));
+					while ((c = br.readLine()) != null) {
+
+						if (i % 43 == 0 && i != 0) {
+							bw.flush();
+							bw.close();
+							docNo++;
+							bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("unichar/h"+docNo+".txt")));
+
+
+						} else if(i!=0) bw.write(System.lineSeparator());
+
+						bw.write(c);
+						i++;
+					}
+
+					bw.flush();
+
+					bw.close();
+					br.close();
+				}
 		};
 	}
 }
